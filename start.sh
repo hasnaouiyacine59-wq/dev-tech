9d487276252d941c42dd8fe393ca10c15cc5f946
oo33 <hasnaouiyacine59@gmail.com>
2026-04-11 22:28:42 +0100

    Use TOR_HOST env var instead of hardcoded 127.0.0.1

--- start.sh
+++ start.sh
@@ -7,9 +7,11 @@ if [ -d ".git" ]; then
   git pull origin main || echo "git pull failed, continuing with existing code"
 fi
 
+TOR_HOST=${TOR_HOST:-127.0.0.1}
+
 # Wait for tor-proxy to be ready
 echo "==> Waiting for Tor...!!!!!!!!"
-until nc -z 127.0.0.1 "${SOCKS_PORT:-9050}" && nc -z 127.0.0.1 "${API_PORT:-5000}"; do
+until nc -z ${TOR_HOST} "${SOCKS_PORT:-9050}" && nc -z ${TOR_HOST} "${API_PORT:-5000}"; do
   sleep 5
 done
 
